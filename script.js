fca8564fcaeab49cf582d1c98f3d18c70cb8bf70
kravchenko-d <kravchenko4@mail.ru>
2023-06-23 21:14:28 +0300

    first commit

--- script.js
+++ script.js
@@ -1,194 +1,211 @@
-// Простые
-   let number = 1234 // Числовой тип
-// объявление инициализация
-let double = 12.43
+const app = document.getElementById("app")
 
-let string = "some string" // Строковый тип
-let string1 = 'some string 1'
+const popup = document.getElementById("popup")
+const popupBody = document.getElementById("popup-body")
 
-let boolean = true // Логический тип
-let boolean1 = false
+const crossWin = document.getElementById("cross-win") // Homework_6
+const zeroWin = document.getElementById("zero-win")
+let crossWinCount = 1
+let zeroWinCount = 1
 
-let undef = undefined // Пустые значения
-// NaN, null
+let player = "cross"  // zero
+const cells = []
+for(let i = 0; i < 9; i++){
+    var cell = document.createElement("div")
+    cell.classList.add("cell")
 
-let symb = Symbol("some") // Символ
+    cell.addEventListener("click", (event) => onClickCell(event, i))
 
-// Составные
-let array = [] // Массив
-
-let obj = {} // Объект
-
-let func = function(a, b ,c) {} // Функция
-let func1 = (a, b ,c) => {} // Стрелочная функция
-function func2(a, b, c) {}
-
-// В JS 8 типов данных
-
-let  value
-
-value = 1432 // Переменную можно инициализировать в процессе
-
-value = "string"
-
-
-const yep = "VALUE" // Константная ссылка на переменную в памяти
-
-// yep = "Other value" // error - нельзя изменять значение константы
-
-const obj1 = {
-    key: "value"
+    app.appendChild(cell)
+    cells.push(cell)
 }
 
-obj1.key // "value"
+const line = document.createElement("div")
 
-obj1.key = "value1"
-obj1.key1 = "value_112"
+line.style.backgroundColor = "red" //"black"
+line.style.width = "0"
+line.style.height = "0"
+line.style.borderRadius = "2px"
+line.style.position = "absolute"
+line.style.transitionDuration = "2s"
 
-console.log(obj1)
+app.appendChild(line)
 
-const arr = []
+popup.addEventListener("click", (event) => 
+{
+    popup.style.display = "none"
 
-console.log("this array - ", arr)
-
-arr.push("string")
-
-console.log("this array - ", arr)
-
-// const num = prompt("Введите число") // Запрос данных у пользователя
-const num = 0// parseInt(prompt("Введите число"))
-console.log(typeof num, num)
-console.log(typeof parseInt(num), num) // parseInt превращает текст в число
-
-let res = ""
-
-if (Number.isNaN(num)){
-    res = "Введено не число"
+    if(cell.classList.contains("cell")){
+        for(let i = 0; i < 9; i++){
+        cells[i].classList.remove("zero", "cross")
+        }
+    }
+    line.style.width = "0"
+    line.style.height = "0"
+    line.style.transform = "none"
 }
-else if(num > 10){
-    res = "Число больше загаданного"
-}
-else if(num === 10){
-    res = "Вы угадали"
-} // === проверяет равенство не только значения, но и его типа, а == проверяет без сравнения типа
-else {
-    res = "Число меньше загаданного"
-}
+) // Homework_6
 
-// alert(res)
-
-const a = 144
-const b = 0.5
-
-let c
-c = a + b
-c = a * b
-c = a - b
-c = a / b
-c = a**b // Степень
-c = a % b // Взятие остатка от деления
-
-console.log(c)
-
-// 1. Запросить два числа
-// 2. Запросить операцию
-// 3. Вернуть ошибку или результат
-
-// // const num1 = +prompt("Введите первое число")
-// // const num2 = +prompt("Введите второе число")
-// // const operation = prompt("Введите операцию")
-
-// //if (Number.isNaN(num1) || Number.isNaN(num2)){
-//     //alert("Некорректные числа")
-// //}
-
-// // else {
-// //     let result
-
-//     // if (operation === "+"){
-//     //     result = num1 + num2
-//     // }
-//     // else if(operation === "-"){
-//     //     result = num1 - num2
-//     // }
-//     // else if(operation === "*"){
-//     //     result = num1 * num2
-//     // }
-//     // else if(operation === "/"){
-//     //     result = num1 / num2
-//     // }
-//     // else if(operation === "**"){
-//     //     result = num1 ** num2
-//     // }
-//     // else if(operation === "%"){
-//     //     result = num1 % num2
-//     // }
+function onClickCell(event, index) {
+    const cell = cells[index]
     
-//     switch(operation){
-//         case "/":
-//             result = num1 / num2
-//             break
-//         case "*":
-//             result = num1 * num2
-//             break
-//         case "+":
-//             result = num1 + num2
-//             break
-//         case "-":
-//             result = num1 - num2
-//             break
-//         case "**":
-//             result = num1 ** num2
-//             break
-//         case "%":
-//             result = num1 % num2
-//             break
-//         default:
-//             result = undefined
-//     }
+    // cells[index].innerHTML = "<span>x</span>"
+    if(!cell.classList.contains("cross")  && !cell.classList.contains("zero")){
+            cell.classList.add(player)
+            checkWin(index, player)
 
-//     // на два кейса можно задавать один брейк
-//     // switch(operation){
-//     //     case "/":
-//     //      case "делить":
-//     //         result = num1 / num2
-//     //         break
-//     //     case "*":
-//     //         result = num1 * num2
-//     //         break
-//     //     default:
-//     //         result = undefined
-//     // }
-
-//     if(result === undefined){
-//         // alert("Некорректная операция")
-//     }
-//     else {
-//        // alert("Результат - " + result)
-//     }
-// }
-
-// for(let i = 0; i < 10; i = i++){ // i = i + 1 === i++
-//     console.log("Итерация №", i)
-// }
-
-let flag = true
-
-while(flag){
-    const n = Math.round(Math.random() * 99 + 1)    // Math.round - округление в ближайшую сторону
-    flag = n % 4 !== 3
-    // if (n % 4 == 3) {
-    //     flag = false
-    // }
-    console.log(n)
+        if(player === "cross") {
+            // cell.innerText = "x"
+            player = "zero"
+        }
+        else if(player === "zero"){
+            // cell.innerText = "o"
+            player = "cross"
+        }        
+    }
 }
 
-let i = 0
+function checkWin(index, player) {
+    let isWin = false
+    let direction
 
-do {
-    console.log("do..while")
-} while(i > 0)
+    const row = Math.floor (index / 3)
+    const column = index % 3
 
-while(i > 0) {
-    console.log("while")
+    const cell1 =cells[row * 3], 
+          cell2 = cells[row * 3 + 1],
+          cell3 = cells[row * 3 + 2]
+
+
+    if(cell1.classList.contains(player) && cell2.classList.contains(player) && cell3.classList.contains(player)) {
+        isWin = true
+        direction = "row"
+    }
+    
+    if(!isWin) {
+        const cell1 =cells[column], 
+              cell2 = cells[column + 3],
+              cell3 = cells[column + 6]
+
+              if(cell1.classList.contains(player) && cell2.classList.contains(player) && cell3.classList.contains(player)) {
+                isWin = true
+                direction = "column"
+            }
+    }
+
+    if(!isWin){
+        let cell1, cell2, cell3
+        if(
+            (row === 0 || row == 2) && (column === 0 || column === 2) ||
+            row === 1 && column === 1
+            ){
+                if(
+                    row === 0 && column === 0 ||
+                    row === 1 && column === 1 ||
+                    row === 2 && column === 2
+                ){
+                    cell1 = cells[0]
+                    cell2 = cells[4]
+                    cell3 = cells[8]
+                    direction = "diagonal-lr"
+                    
+                }
+
+                if(
+                    row === 0 && column === 2 ||
+                    row === 1 && column === 1 ||
+                    row === 2 && column === 0
+                ){
+                    cell1 = cells[2]
+                    cell2 = cells[4]
+                    cell3 = cells[6]
+                    direction = "diagonal-rl"
+                }
+
+                if(cell1.classList.contains(player) && cell2.classList.contains(player) && cell3.classList.contains(player)) {
+                    isWin = true
+                }
+        }
+    }
+
+    if(isWin){
+        crossLineWin(row, column, direction)
+        showWinPopup(player)
+        if(player === "cross"){
+            crossWin.innerText = crossWinCount++
+        }
+        else{
+            zeroWin.innerText = zeroWinCount++
+        } // Homework_6
+        // setTimeout(
+        //     () => {
+        //         crossLineWin(row, column, "diagonal-rl")
+        //         // alert(`Win is ${player}`)
+        //     },
+        //     100
+        // )        
+    }
+}
+
+function crossLineWin(row, column, direction){
+    // const line = document.createElement("div")
+
+    // line.style.backgroundColor = "red" //"black"
+    // line.style.width = "90%"
+    // line.style.height = "3px"
+    // line.style.borderRadius = "2px"
+    // line.style.position = "absolute"
+    // line.style.transition = "width 1s"
+
+    switch(direction){
+        case "row":
+            line.style.transitionProperty = "width"
+            line.style.top = row * 100 + 50 - 1.5 + "px"
+            line.style.left = "5%"
+            // row === 0 => 0 * 100 + 50 = 50px
+            // row === 1 => 1 * 100 + 50 = 150px
+            // row === 2 => 2 * 100 + 50 = 250px
+
+            line.style.width = "90%"
+            line.style.height = "3px"
+
+            break;
+        case "column":
+            line.style.transitionProperty = "height"
+            line.style.left = column * 100 + 50 - 1.5 + "px"
+            line.style.top = "5%"
+            
+            line.style.width = "3px"
+            line.style.height = "90%"
+
+            break;
+        case "diagonal-lr":
+            line.style.transitionProperty = "height"
+            line.style.width = "3px"
+            line.style.height = 90 * Math.sqrt(2) + "%"
+
+            line.style.left = "calc(50% - 1.5px)"
+            line.style.top = "calc(5% - 50px - 6px)"
+
+            line.style.transform = "rotate(-45deg)"
+
+            break;
+        case "diagonal-rl":
+            line.style.transitionProperty = "height"
+            line.style.width = "3px"
+            line.style.height = 90 * Math.sqrt(2) + "%"
+
+            line.style.left = "calc(50% - 1.5px)"
+            line.style.top = "calc(5% - 50px - 6px)"
+
+            line.style.transform = "rotate(45deg)"
+            break;
+    }
+
+    // app.appendChild(line)
+}
+
+function showWinPopup(player){
+    popupBody.innerText = `Winner is ${player}` // "Winner is " + player
+    popup.style.display = "flex"
 }
